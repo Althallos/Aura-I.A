97284a6807d0dcf656d495aac0c565a05efc6387
ZhouZKZ <roberto16092004@gmail.com>
2024-01-06 19:09:46 -0300

    Update main.py

--- projeto/main.py
+++ projeto/main.py
@@ -1,12 +1,13 @@
 from voz import falar, microfone
 from comandos import executar_comando
 
-
+falar('Olá, Meu Nome é Aura Uma I.A Criada Para Te Ajudar.', True)
+falar('Para Ver Os Possíveis Comandos, Fale AJUDA!', True)
 def listen_command():
     while True:
         try:
             comando = microfone()
-            falar(comando, True)
+            print(comando, True)
             comando = comando.replace('Aura', '')
             return comando
         except Exception as e:
@@ -16,5 +17,5 @@ while True:
     comando = executar_comando(listen_command())
     #   Desligar a I.A
     if('desligar' == comando):
-
         break
+    
